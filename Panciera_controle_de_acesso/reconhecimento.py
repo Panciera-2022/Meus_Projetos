3b81ae1c1411fbac18fbbede7e4e37d9962d5586
Paciera <maicontgti@gmail.com>
2022-10-29 07:15:08 -0300

    subindo
    ok

--- Panciera_controle_de_acesso/reconhecimento.py
+++ Panciera_controle_de_acesso/reconhecimento.py
@@ -11,3 +11,30 @@ desenho = mp.solutions.drawing_utils
 
 
 
+while True:
+    # 1-Ler as informações da webcam
+    verificador, imagem = webcam.read()
+    if not verificador:
+        break
+    # 2-Reconhecer os rostos que tem ali dentro
+    lista_rostos = reconhecedor_rostos.process(imagem)
+
+    if lista_rostos.detections:
+        for rosto in lista_rostos.detections:
+            # 3-Desenhar os rostos na imagem
+            desenho.draw_detection(imagem, rosto)
+    cv2.imshow('Rostos', imagem)
+
+    # 4-Quando apertar ESC, sair do loop
+    if cv2.waitKey(5) == 27:
+        break
+
+
+
+
+
+
+
+
+
+
