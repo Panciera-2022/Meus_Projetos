6d42bd8969f6acd8c123e51a0dd9318fb462d86e
Paciera <maicontgti@gmail.com>
2022-11-18 09:59:49 -0300

    ok
    ok

--- App Celular/myfirebase.py
+++ App Celular/myfirebase.py
@@ -7,16 +7,21 @@ class MyFirebase():
 
     def criar_conta(self, email, senha):
         link = f"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={self.API_KEY}"
-
-        info = {"email": email, "password": senha, "returnSecureToken": True}
+        print(email, senha)
+        info = {"email": email,
+                "password": senha,
+                "returnSecureToken": True}
         requisicao = requests.post(link, data=info)
         requisicao_dic = requisicao.json()
 
         if requisicao.ok:
-            print("usuario criado")
-            id_token = requisicao_dic["idToken"]
+            print("Usuário Criado")
+            # requisicao_dic["idToken"] -> autenticação
+            # requisicao_dic["refreshToken"] -> token que mantém o usuário logado
+            # requisicao_dic["localId"] -> id_usuario
             refresh_token = requisicao_dic["refreshToken"]
             local_id = requisicao_dic["localId"]
+            id_token = requisicao_dic["idToken"]
 
             meu_aplicativo = App.get_running_app()
             meu_aplicativo.local_id = local_id
@@ -25,17 +30,17 @@ class MyFirebase():
             with open("refreshtoken.txt", "w") as arquivo:
                 arquivo.write(refresh_token)
 
-            req_id = requests.get("https://aplicativovendas-f1070-default-rtdb.firebaseio.com/proximo_id_vendedor.json")
+            req_id = requests.get(f"https://aplicativovendas-f1070-default-rtdb.firebaseio.com/proximo_id_vendedor.json?auth={id_token}")
             id_vendedor = req_id.json()
 
-            link = f"https://aplicativovendas-f1070-default-rtdb.firebaseio.com/{local_id}.json"
+            link = f"https://aplicativovendas-f1070-default-rtdb.firebaseio.com/{local_id}.json?auth={id_token}"
             info_usuario = f'{{"avatar": "foto1.png", "equipe": "", "total_vendas": "0", "vendas": "", "id_vendedor": "{id_vendedor}"}}'
             requisicao_usuario = requests.patch(link, data=info_usuario)
 
-            # atualizar o valor do proximo id vendedor
+            # atualizar o valor do proximo_id_vendedor
             proximo_id_vendedor = int(id_vendedor) + 1
             info_id_vendedor = f'{{"proximo_id_vendedor": "{proximo_id_vendedor}"}}'
-            requests.patch("https://aplicativovendas-f1070-default-rtdb.firebaseio.com/.json", data=info_id_vendedor)
+            requests.patch(f"https://aplicativovendas-f1070-default-rtdb.firebaseio.com/.json?auth={id_token}", data=info_id_vendedor)
 
             meu_aplicativo.carregar_infos_usuario()
             meu_aplicativo.mudar_tela("homepage")
@@ -46,10 +51,13 @@ class MyFirebase():
             pagina_login = meu_aplicativo.root.ids["loginpage"]
             pagina_login.ids["mensagem_login"].text = mensagem_erro
             pagina_login.ids["mensagem_login"].color = (1, 0, 0, 1)
+        print(requisicao_dic)
 
     def fazer_login(self, email, senha):
         link = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={self.API_KEY}"
-        info = {"email": email, "password": senha, "returnSecureToken": True}
+        info = {"email": email,
+                "password": senha,
+                "returnSecureToken": True}
         requisicao = requests.post(link, data=info)
         requisicao_dic = requisicao.json()
 
@@ -77,14 +85,12 @@ class MyFirebase():
 
     def trocar_token(self, refresh_token):
         link = f"https://securetoken.googleapis.com/v1/token?key={self.API_KEY}"
-
         info = {
-            "grant_type": "refresh_token",
+            "grant_type":  "refresh_token",
             "refresh_token": refresh_token
         }
         requisicao = requests.post(link, data=info)
         requisicao_dic = requisicao.json()
         local_id = requisicao_dic["user_id"]
         id_token = requisicao_dic["id_token"]
-
         return local_id, id_token
